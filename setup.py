bfc42aa631f4530ba96cb23505c88173d668783e
Francisco Vasconcelos <f.xavier.vasconcelos@gmail.com>
2024-03-10 11:10:23 +0000

    building and packaging gasparse

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ import sys
 name = "gasparse"
 macros = [("INCLUDE_GENCODE",None)]
 setup(  name=name, 
-        version="0.1",
+        version="0.0.0a1", # Should have used git tag for the version
+        # packages=["gasparse"],
         ext_modules=[Extension(name,["src/gasparse.c","src/multivector_object.c","src/multivector_types.c","src/multivector_large.c","src/multivector_gen.c","src/common.c"],\
         extra_compile_args = ["-O0", "-mpopcnt","-g3"],define_macros=macros)],
         description="A python library written entirely in C for Geometric Algebras to deal with sparse multivector arrays",
         long_description = open('README.md').read(),
-        
         license='MIT',
         author="Francisco Mendia",
-        author_email="francisco.mendia.99@gmail.com"
+        author_email="francisco.mendia.99@gmail.com",
+        python_requires=">=3.7"
 )
